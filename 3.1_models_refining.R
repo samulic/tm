ec38b5e01e9a0710b0e8280b94157e648034da6f
samulic <samuli.cal@gmail.com>
2019-02-09 14:08:25 +0100

    custom hyperparameters tuning

--- 3.1_models_refining.R
+++ 3.1_models_refining.R
@@ -8,11 +8,18 @@ load("Project/Data/svm_result_default.RData")
 #load("Project/Data/results_default.RData) # for the future
 ## Use variable importance from Decision Tree to select variables
 ## to be used in KNN which is prone to overfitting
-
+train_knn_classifier <- function(train_df, metric, control) {
+  tic("KNN")
+  set.seed(7)
+  model <- train(Topic~., data=train_df, method="knn", metric=metric, 
+                 trControl=control, tuneGrid = expand.grid(.k = 2:4))
+  toc()
+  return(model)
+}
 # Variable importance from the final decision tree model
 imps <- as.matrix(varImp(dt_model)$importance)
 # How many variable ?
-threshold <- 1:100
+threshold <- 15:50
 nimps <- c()
 for (t in threshold) {
   nimps <- c(nimps, length(imps[imps >= t]))
@@ -27,3 +34,10 @@ var <- c(var, "Topic") # add target class
 # Fit KNN on most important variables only
 small_knn_model <- train_knn_classifier(train_df[, var], metric, control)
 
+knn_model_357 <- small_knn_model
+#knn_model_358 <- small_knn_model # t = 57
+# t = 22
+
+results <- resamples(list(n358 = knn_model_358, 
+                          n357 = knn_model_357))
+bwplot(results)
