c0d3432cd38b103717ae31a9c59caeaed3de0f73
samulic <samuli.cal@gmail.com>
2019-02-11 23:24:51 +0100

    final

--- 3_models.R
+++ 3_models.R
@@ -68,10 +68,10 @@ control <- trainControl(method = "cv", number = 5, classProbs = TRUE,
                         summaryFunction = twoClassSummary) # for ROC metric
 metric <- "ROC" #"Accuracy" alternatively
 
-save(list = c("control", "metric", "train_nn_classifier",
-              "train_dt_classifier", "train_dt_classifier",
-              "train_knn_classifier", "train_svm_classifier"),
-     file = paste0(folder, "Data/models_declaration.RData"))
+# save(list = c("control", "metric", "train_nn_classifier",
+#               "train_dt_classifier", "train_dt_classifier",
+#               "train_knn_classifier", "train_svm_classifier"),
+#      file = paste0(folder, "Data/models_declaration.RData"))
 # Classifiers
 nn_model <- train_nn_classifier(train_df, metric, control)
 dt_model <- train_dt_classifier(train_df, metric, control)
@@ -96,22 +96,41 @@ dotplot(results)
 
 # Test predictions
 dt_predictions <- predict(dt_model, newdata = test_df)
+dt_predictions_prob <- predict(dt_model, newdata = test_df, type = "prob")
 dt_confusion_matrix <- confusionMatrix(table(dt_predictions, test_df$Topic))
 cat('Decision Tree test accuracy: ', unname(dt_confusion_matrix$overall[1]), '\n')
 
 svm_predictions <- predict(svm_model, newdata = test_df)
+svm_predictions_prob <- predict(svm_model, newdata = test_df, type = "prob")
 svm_confusion_matrix <- confusionMatrix(table(svm_predictions, test_df$Topic))
 cat('SVM test accuracy: ', unname(svm_confusion_matrix$overall[1]), '\n')
 
 knn_predictions <- predict(knn_model, newdata = test_df)
+knn_predictions_prob <- predict(knn_model, newdata = test_df, type = "prob")
 knn_confusion_matrix <- confusionMatrix(table(knn_predictions, test_df$Topic))
 cat('KNN test accuracy: ', unname(knn_confusion_matrix$overall[1]), '\n')
 
-rf_predictions <- predict(rf_model, newdata = test_df)
-rf_confusion_matrix <- confusionMatrix(table(rf_predictions, test_df$Topic))
-cat('Random Forest test accuracy: ', unname(rf_confusion_matrix$overall[1]), '\n')
-
 nn_predictions <- predict(nn_model, newdata = test_df)
+nn_predictions_prob <- predict(nn_model, newdata = test_df, type = "prob")
 nn_confusion_matrix <- confusionMatrix(table(nn_predictions, test_df$Topic))
 cat('Neural Networks test accuracy: ', unname(nn_confusion_matrix$overall[1]), '\n')
+
+
+## ROC Curve ( for binary Topic )
+for_lift <- data.frame(Class = test_df$Topic,
+                      DT = dt_predictions_prob$Sci,
+                      SVM = svm_predictions_prob$Sci,
+                      #KNN = knn_predictions_prob$Sci,
+                      NN  = nn_predictions_prob$Sci)
+#lift <- lift(Class ~ DT + SVM + KNN + NN, data = for_lift, )
+lift <- lift(Class ~ DT + SVM + NN, data = for_lift, class = "Sci")
+
+library(ggplot)
+
+ggplot(lift$data) + 
+  geom_line(aes(1 - Sp , Sn, color = liftModelVar), size = 1.5) +
+  scale_color_discrete(guide = guide_legend(title = "Classifier")) +
+  theme(text = element_text(size=15)) +
+  labs(y="Sens", x = "1 - Spec") +
+  ggtitle("ROC curves on test set")
 
